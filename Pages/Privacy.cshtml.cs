6b2b1fad7c0af938be1fba46f3f57dc4d6ebfd3a
joão <joaocarrilholoza@gmail.com>
2026-04-19 10:53:03 -0300

    add Projeto aspnet com razor pages

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace frontend_sistema_odontologico.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
